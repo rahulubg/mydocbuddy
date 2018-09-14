4c3e8be76ccb86c011e6b02b3e0e38e2e28acba6
rahulubg <Rahulubgade@hotmail.com>
2018-09-14 05:54:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create adapter
 const adapter = new BotFrameworkAdapter({ 
-    appId: process.env.MICROSOFT_APP_ID, 
-    appPassword: process.env.MICROSOFT_APP_PASSWORD 
+    appId: c8cd0ad4-daf3-4ad8-bec4-3aa34684c4c4, 
+    appPassword: rbLTUPPM627bfowdD03)_#| 
 });
 
 // Add conversation state middleware
