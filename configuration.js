d5e70b658086b1fcc27cb6f2ac00837e7c4f6b74
rahulubg <Rahulubgade@hotmail.com>
2018-09-14 03:37:40 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,11 +1,11 @@
 var _config = {
     CHAT_CONNECTOR: {
-        APP_ID: process.env.MICROSOFT_APP_ID, //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
-        APP_PASSWORD: process.env.MICROSOFT_APP_PASSWORD
+        APP_ID: c8cd0ad4-daf3-4ad8-bec4-3aa34684c4c4, //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
+        APP_PASSWORD: cwdsmSYV5346(gaAWGO5[+;
     },
     COMPUTER_VISION_SERVICE: {
         API_URL: "https://api.projectoxford.ai/vision/v1.0/",
-        API_KEY: process.env.VISION_API_KEY  //You can obtain an COGNITIVE SERVICE API KEY: https://www.microsoft.com/cognitive-services/en-us/pricing
+        API_KEY: ef46e2e021394aab92ca2d2eb57930b6  //You can obtain an COGNITIVE SERVICE API KEY: https://www.microsoft.com/cognitive-services/en-us/pricing
     }
 };
 exports.CONFIGURATIONS = _config;
